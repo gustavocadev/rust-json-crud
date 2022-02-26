29f20f7fa87f1fdb2a351b78b47c3657a8348e35
gustavocadev <churaalaygustavo11@gmail.com>
2022-02-25 19:15:16 -0500

    I Added some interactive prompts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate colour;
 use colored::*;
+use dialoguer::{theme::ColorfulTheme, Input, Select};
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::error::Error;
@@ -69,12 +70,17 @@ fn input_str(prompt: &str) -> String {
 // create a menu
 fn menu() -> u8 {
     yellow!("Welcome to the user manager\n");
-    cyan!("1. Agregar un usuario\n");
-    cyan!("2. Eliminar a un usuario\n");
-    cyan!("3. Editar usuario\n");
-    cyan!("4. Listar usuarios\n");
-    cyan!("5. Salir\n");
-    let choice = input_int("Ingrese una opción: ");
+    let choice = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Seleccione una opción: ")
+        .default(0)
+        .item("Agregar un usuario")
+        .item("Eliminar a un usuario")
+        .item("Editar usuario")
+        .item("Listar usuarios")
+        .item("Salir")
+        .interact()
+        .unwrap();
+    let choice = choice as u8 + 1;
     choice
 }
 
@@ -87,8 +93,14 @@ fn main() {
         let mut data: JsonStruct = read_json("./db.json").unwrap();
 
         if option == 1 {
-            let name = input_str("Enter your name: ");
-            let age = input_int("Enter your age: ");
+            let name = Input::<String>::with_theme(&ColorfulTheme::default())
+                .with_prompt("Ingrese el nombre del usuario")
+                .interact()
+                .unwrap();
+            let age = Input::<u8>::with_theme(&ColorfulTheme::default())
+                .with_prompt("Ingrese la edad del usuario")
+                .interact()
+                .unwrap();
 
             let id = Uuid::new_v4().to_string();
 
@@ -96,7 +108,10 @@ fn main() {
 
             write_json(&data, "./db.json").unwrap();
         } else if option == 2 {
-            let name = input_str("Enter your name: ");
+            let name = Input::<String>::with_theme(&ColorfulTheme::default())
+                .with_prompt("Ingrese el nombre del usuario")
+                .interact()
+                .unwrap();
 
             for (i, user) in data.users.iter().enumerate() {
                 if user.name == name {
@@ -107,11 +122,20 @@ fn main() {
 
             write_json(&data, "./db.json").unwrap();
         } else if option == 3 {
-            let name = input_str("Enter your name: ");
+            let name = Input::<String>::with_theme(&ColorfulTheme::default())
+                .with_prompt("Ingrese el nombre del usuario")
+                .interact()
+                .unwrap();
             for (i, user) in data.users.iter().enumerate() {
                 if user.name == name {
-                    let new_name = input_str("Enter your new name: ");
-                    let new_age = input_int("Enter your new age: ");
+                    let new_name = Input::<String>::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Ingrese el nuevo nombre del usuario")
+                        .interact()
+                        .unwrap();
+                    let new_age = Input::<u8>::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Ingrese la nueva edad del usuario")
+                        .interact()
+                        .unwrap();
                     data.users[i].name = new_name;
                     data.users[i].age = new_age;
                     break;
@@ -121,7 +145,7 @@ fn main() {
         } else if option == 4 {
             for user in data.users.iter() {
                 println!("{}", "-----------------".green());
-                println!("Mi nombre es {}", user.name.red());
+                println!("Mi nombre es {}", user.name.yellow());
                 println!("Mi edad es {}", user.age.to_string().yellow());
             }
         } else if option == 5 {
